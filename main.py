070e3f57a3bd7fba808c86d6f53375d299ab25ca
Gustav Gille <gustav.gille@icloud.com>
2023-01-20 15:38:49 +0100

    Clustered for now but gives the correct output

--- main.py
+++ main.py
@@ -13,18 +13,42 @@ def createCorrectLifeExpectancy(year):
     return df2.loc[df["Year"] == year]
 
 
+def scatterPlot():
+    global row
+    for row in enumerate(correct_GDP_table.merge(correct_Life_Table, on="Entity").columns):
+        plt.scatter(x, y)
+        print(type(x))
+    giveScatterPlotName()
+
+
+def giveScatterPlotName():
+    global i, row
+    for i, row in enumerate(correct_GDP_table.merge(correct_Life_Table, on="Entity").iterrows()):
+        plt.annotate(row[1][0], (x_values[i], y_values[i]))
+
+
+def createScatterIndex():
+    global x_values, i, y_values
+    x_values = []
+    for i in x:
+        x_values.append(i)
+    y_values = []
+    for i in y:
+        y_values.append(i)
+
+
 if __name__ == '__main__':
     correct_GDP_table = createCorrectGDPTable(2020)
     correct_Life_Table = createCorrectLifeExpectancy(2020)
-    correct_GDP_table.merge(correct_Life_Table, on="Entity")
-    correct_Life_Table.merge(correct_GDP_table, on="Entity")
 
-    print(correct_GDP_table.merge(correct_Life_Table, on="Entity"),
-          correct_Life_Table.merge(correct_GDP_table, on="Entity"))
+    x = correct_Life_Table.merge(correct_GDP_table, on="Entity")["Life expectancy at birth (historical)"]
+    y = correct_GDP_table.merge(correct_Life_Table, on="Entity")["GDP per capita (constant 2015 US$)"]
 
-    for col in correct_GDP_table.merge(correct_Life_Table, on="Entity").columns:
-        plt.scatter(correct_Life_Table.merge(correct_GDP_table, on="Entity")["Life expectancy at birth (historical)"],
-                    correct_GDP_table.merge(correct_Life_Table, on="Entity")["GDP per capita (constant 2015 US$)"])
-    plt.xlabel('GDP')
-    plt.ylabel('Life Expectancy')
+    createScatterIndex()
+
+    scatterPlot()
+#    print(y_values)
+    plt.yscale("log")
+    plt.xlabel('Life Expectancy')
+    plt.ylabel('GDP per capita')
     plt.show()
