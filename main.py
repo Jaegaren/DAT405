618b31a94ac2e50d1c994db78b369adc83fa3f6a
Gustav Gille <gustav.gille@icloud.com>
2023-01-20 13:55:34 +0100

    asdfg

--- main.py
+++ main.py
@@ -16,9 +16,15 @@ def createCorrectLifeExpectancy(year):
 if __name__ == '__main__':
     correct_GDP_table = createCorrectGDPTable(2020)
     correct_Life_Table = createCorrectLifeExpectancy(2020)
+    correct_GDP_table.merge(correct_Life_Table, on="Entity")
+    correct_Life_Table.merge(correct_GDP_table, on="Entity")
 
-    correct_GDP_table.merge(correct_Life_Table, on="Code")
-    correct_Life_Table.merge(correct_GDP_table, on="Code")
+    print(correct_GDP_table.merge(correct_Life_Table, on="Entity"),
+          correct_Life_Table.merge(correct_GDP_table, on="Entity"))
 
-    print(correct_GDP_table.merge(correct_Life_Table, on="Code"),
-          correct_Life_Table.merge(correct_GDP_table, on="Code"))
+    for col in correct_GDP_table.merge(correct_Life_Table, on="Entity").columns:
+        plt.scatter(correct_Life_Table.merge(correct_GDP_table, on="Entity")["Life expectancy at birth (historical)"],
+                    correct_GDP_table.merge(correct_Life_Table, on="Entity")["GDP per capita (constant 2015 US$)"])
+    plt.xlabel('GDP')
+    plt.ylabel('Life Expectancy')
+    plt.show()
