c11d331db8bba3f778492f14817b2ad28d74c60d
Gustav Gille <gustav.gille@icloud.com>
2023-01-20 11:49:20 +0100

    correctGDP table

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-def createCorrectTable():
+def createCorrectGDPTable(year):
     df = pd.read_csv('/Users/gille/Documents/GitHub/DAT405/files/gdp-per-capita-in-us-dollar-world-bank.csv')
-    df.loc(df["Year"] == 2020)
-    print(df.to_string())
-
+    return df.loc[df["Year"] == year]
 
 if __name__ == '__main__':
-    createCorrectTable()
+    correct_table = createCorrectGDPTable(2020)
+    print(correct_table)
 
