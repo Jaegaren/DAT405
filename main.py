e22ac19fee9366737632512de5e45c0436bf907f
Gustav Gille <gustav.gille@icloud.com>
2023-01-20 11:39:34 +0100

    sedf

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-def print_hi(name):
-    df = pd.read_csv('')
+def createCorrectTable():
+    df = pd.read_csv('/Users/gille/Documents/GitHub/DAT405/files/gdp-per-capita-in-us-dollar-world-bank.csv')
+    df.loc(df["Year"] == 2020)
+    print(df.to_string())
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    createCorrectTable()
 
