d021bf06a398991fbb69d74b43f231902dab6bc0
Gustav Gille <gustav.gille@icloud.com>
2023-01-23 21:35:10 +0100

    Made the code readable.

--- AI assignment 1/main.py
+++ AI assignment 1/main.py
@@ -4,47 +4,38 @@ import numpy as np
 
 
 def createCorrectGDPTable(year):
-    df = pd.read_csv('/Users/jacobwesterberg/PycharmProjects/DAT405/files/gdp-per-capita-in-us-dollar-world-bank.csv')
+    df = pd.read_csv('/Users/gille/Documents/GitHub/DAT405/files/gdp-per-capita-in-us-dollar-world-bank.csv')
     return df.loc[df["Year"] == year]
 
 
 def createCorrectLifeExpectancy(year):
-    df = pd.read_csv("/Users/jacobwesterberg/PycharmProjects/DAT405/files/life-expectancy.csv")
+    df = pd.read_csv("/Users/gille/Documents/GitHub/DAT405/files/life-expectancy.csv")
     df2 = df.dropna()
     return df2.loc[df["Year"] == year]
 
 
 def scatterPlot():
-    global row
+    for index, row in countryIndex.items():
+        plt.scatter(x[index], y[index], color="blue", s=10)
+    giveScatterPlotNames()
+
+
+def scatterPlotCountriesWithHighLivingExp():
     sigmaAboveMedian = calculateSigmaAboveMedian()
-    for index, col in enumerate(correct_GDP_table.merge(correct_Life_Table, on="Entity").iterrows()):
-        if lifeExp[index] > sigmaAboveMedian:
-            plt.scatter(x[index], y[index], color="green", s =10)
-        else:
-            plt.scatter(x[index], y[index], color="red", s=10)
-    giveScatterPlotName()
+    for index, col in countryIndex.items():
+        if x.at[index] > sigmaAboveMedian:
+            plt.scatter(x[index], y[index], color="green", s=10)
+    giveScatterPlotNames()
 
 
-def giveScatterPlotName():
-    global i, row
-    for i, row in enumerate(correct_GDP_table.merge(correct_Life_Table, on="Entity").iterrows()):
-        plt.annotate(row[1][0], (lifeExp[i], GDPCap[i] ), ha= "center")
+def giveScatterPlotNames():
+    for index, genericTable in countryIndex.items():
+        plt.annotate(countryIndex[index], (x[index], y[index]), ha="center")
 
-
-def createScatterIndex():
-    global lifeExp, i, GDPCap
-    lifeExp = []
-    for i in x:
-        lifeExp.append(i)
-    GDPCap = []
-    for i in y:
-        GDPCap.append(i)
 
 def calculateSigmaAboveMedian():
-    stdLifeExpectancy = np.std(x)
-    medianLifeExpectancy = np.median(x)
-    sigmaAboveMedian = medianLifeExpectancy + stdLifeExpectancy
-    return sigmaAboveMedian
+    return np.median(x) + np.std(x)
+
 
 
 if __name__ == '__main__':
@@ -53,11 +44,8 @@ if __name__ == '__main__':
 
     x = correct_Life_Table.merge(correct_GDP_table, on="Entity")["Life expectancy at birth (historical)"]
     y = correct_GDP_table.merge(correct_Life_Table, on="Entity")["GDP per capita (constant 2015 US$)"]
-
-    createScatterIndex()
-
-    scatterPlot()
-    #   print(y_values)
+    countryIndex = correct_Life_Table.merge(correct_GDP_table, on="Entity")["Entity"]
+    scatterPlotCountriesWithHighLivingExp()
     plt.yscale("log")
     plt.xlabel('Life Expectancy')
     plt.ylabel('GDP per capita')
