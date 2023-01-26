4b3e6a5fbabb107eb4805feb38591f1c8ec4c2dc
Gustav Gille <gustav.gille@icloud.com>
2023-01-26 11:32:40 +0100

    Added code to remove outliers, added some comments to the code and a way to annotate the scatter points.

--- Ai assignment 2/main2.py
+++ Ai assignment 2/main2.py
@@ -9,27 +9,35 @@ def readHouseTable():
     return df
 
 
-def calculateAndPlotLineRegression(x, y):
-    xReg = x.values.reshape(-1, 1)
-    yReg = y.values.reshape(-1, 1)
+def calculateAndPlotLineRegression(xValues, yValues):
+    xReg = xValues.values.reshape(-1, 1)  # These are made to reshape
+    yReg = yValues.values.reshape(-1, 1)  # the data for LinearRegression()
     lineRegression = LinearRegression()
     lineRegression.fit(xReg, yReg)
-    y_pred = lineRegression.predict(xReg)
-    plt.plot(x, y_pred, color="red")
+    y_pred = lineRegression.predict(xReg)  # Prediction
+    plt.plot(xValues, y_pred, color="red")
 
 
-# def plotTwoVariables(x, y):
+def annotateHousesToPandaIndex(indexTable, x, y):
+    for index, genericTable in indexTable.items():
+        plt.annotate(indexTable[index] - 1, (x[index], y[index]))  # minus one to account for panda 0-indexed
+
+
+def dropOutliers(table, dropHouses):
+    return table.drop(dropHouses)
 
 
 if __name__ == '__main__':
     houseTable = readHouseTable()
-    x = houseTable.iloc[:, 1]  # ID index
-    y = houseTable.iloc[:, 6]  # Selling Price index
-    index = houseTable.loc[:, "ID"]
+    houseTable = dropOutliers(houseTable, [40, 45, 9, 24])
+    x = houseTable.iloc[:, 1]  # Living_area index
+    y = houseTable.iloc[:, 6]  # Selling_price index
+    houseIndex = houseTable.loc[:, "ID"]
 
     plt.xlabel("Living Area")
-    plt.ylabel("Selling price")
+    plt.ylabel("Selling Price")
 
+    annotateHousesToPandaIndex(houseIndex, x, y)
     plt.scatter(x, y)
     calculateAndPlotLineRegression(x, y)
-    plt.show()
+    #plt.show()
