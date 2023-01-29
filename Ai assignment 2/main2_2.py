45658ec150904b4ef8991add6b53412ef0d6955a
Jaegaren <jacob.s.westerberg@gmail.com>
2023-01-29 21:51:23 +0100

    confusion matrix

--- Ai assignment 2/main2_2.py
+++ Ai assignment 2/main2_2.py
@@ -1,5 +1,6 @@
-from matplotlib import pyplot as plt
 import numpy as np
+from sklearn import metrics
+import matplotlib.pyplot as plt
 
 
 from sklearn.datasets import load_iris
@@ -12,3 +13,11 @@ from sklearn.datasets import load_iris
 
 if __name__ == '__main__':
     print(load_iris())
+    actual = np.random.binomial(1, 0.9, size=1000)
+    predicted = np.random.binomial(1, 0.9, size=1000)
+    confusion_matrix = metrics.confusion_matrix(actual, predicted)
+
+    cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=[False, True])
+
+    cm_display.plot()
+    plt.show()
