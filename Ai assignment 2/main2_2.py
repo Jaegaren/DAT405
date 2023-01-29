cfea4606de5d362951632c458140a5e5b84a7068
Jaegaren <jacob.s.westerberg@gmail.com>
2023-01-29 23:09:37 +0100

    confusion matrix

--- Ai assignment 2/main2_2.py
+++ Ai assignment 2/main2_2.py
@@ -1,9 +1,11 @@
 import numpy as np
 from sklearn import metrics
 import matplotlib.pyplot as plt
-
-
 from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
 
 
 
@@ -12,12 +14,24 @@ from sklearn.datasets import load_iris
 
 
 if __name__ == '__main__':
-    print(load_iris())
-    actual = np.random.binomial(1, 0.9, size=1000)
-    predicted = np.random.binomial(1, 0.9, size=1000)
-    confusion_matrix = metrics.confusion_matrix(actual, predicted)
+    iris = load_iris()
+    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=0)
+    logreg = LogisticRegression()
+    logreg.fit(X_train, y_train)
+    y_prediction = logreg.predict(X_test)
 
-    cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=[False, True])
+    conf_mat = confusion_matrix(y_test, y_prediction)
 
-    cm_display.plot()
-    plt.show()
+    print(conf_mat)
+    #conf_mat.plot()
+    #plt.show()
+
+
+    #actual = np.random.binomial(1, 0.9, size=1000)
+    #predicted = np.random.binomial(1, 0.9, size=1000)
+    #confusion_matrix = metrics.confusion_matrix(actual, predicted)
+
+    #cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=[False, True])
+
+    #cm_display.plot()
+    #plt.show()
