308d0cd3e454be52b4577cc7bc52f358681925fa
Jaegaren <jacob.s.westerberg@gmail.com>
2023-02-20 16:48:22 +0100

    new assignment

--- AIassignment5/main5.2.py
+++ AIassignment5/main5.2.py
@@ -15,10 +15,21 @@ R = np.zeros((length, width))
 R[1, 1] = 10
 
 # define the initial value function
-V = np.zeros((length, width))
+#V = np.zeros((length, width))
+
+# define the initial value function
+#V = np.ones((length, width))
+
+# define the initial value function
+V = np.ones((length, width))
+V[0, 1] = 500
+V[2, 0] = 9
+V[1, 0] = np.pi*542
 
 # define the probability
-p = 0.8
+prob_moving = 0.8
+prob_staying = 1 - prob_moving
+
 
 # define the epsilon
 epsilon = 0.0001
@@ -26,22 +37,23 @@ epsilon = 0.0001
 def value_iteration():
     while True:
         V_old = np.copy(V)
+        print(V_old)
         for i in range(length):
             for j in range(width):
                 action_val = []
                 if i > 0:
                     # checks if north is possible
-                    action_val.append(p * (R[i - 1, j] + gamma * V_old[i - 1, j]))
+                    action_val.append(prob_moving * (R[i - 1, j] + gamma * V_old[i - 1, j]))
                 if i < length - 1:
                     # checks if south is possible
-                    action_val.append(p * (R[i + 1, j] + gamma * V_old[i + 1, j]))
+                    action_val.append(prob_moving * (R[i + 1, j] + gamma * V_old[i + 1, j]))
                 if j > 0:
                     # checks if west is possible
-                    action_val.append(p * (R[i, j - 1] + gamma * V_old[i, j - 1]))
+                    action_val.append(prob_moving * (R[i, j - 1] + gamma * V_old[i, j - 1]))
                 if j < width - 1:
                     # checks if east is possible
-                    action_val.append(p * (R[i, j + 1] + gamma * V[i, j + 1]))
-                action_val.append(p * (R[i, j] + gamma * V[i, j]))
+                    action_val.append(prob_moving * (R[i, j + 1] + gamma * V[i, j + 1]))
+                action_val.append(prob_moving * (R[i, j] + gamma * V[i, j]))
                 V[i, j] = max(action_val)
         if np.sum(np.abs(V - V_old)) <= epsilon:
             break
@@ -51,14 +63,14 @@ def value_iteration():
         for j in range(width):
             action_val = []
             if i > 0:
-                action_val.append(p * (R[i - 1, j] + gamma * V[i - 1, j]))
+                action_val.append(prob_moving * (R[i - 1, j] + gamma * V[i - 1, j]))
             if i < length - 1:
-                action_val.append(p * (R[i + 1, j] + gamma * V[i + 1, j]))
+                action_val.append(prob_moving * (R[i + 1, j] + gamma * V[i + 1, j]))
             if j > 0:
-                action_val.append(p * (R[i, j - 1] + gamma * V[i, j - 1]))
+                action_val.append(prob_moving * (R[i, j - 1] + gamma * V[i, j - 1]))
             if j < width - 1:
-                action_val.append(p * (R[i, j + 1] + gamma * V[i, j + 1]))
-            action_val.append(p * (R[i, j] + gamma * V[i, j]))
+                action_val.append(prob_moving * (R[i, j + 1] + gamma * V[i, j + 1]))
+            action_val.append(prob_moving * (R[i, j] + gamma * V[i, j]))
 
             pi[i, j] = action_val.index(max(action_val))
 
--- AIassignment5/main5.3.py
+++ AIassignment5/main5.3.py
@@ -2,51 +2,65 @@
 import numpy as np
 
 # define the reward matrix
-R = np.array([[0,0,0], [0,10,0], [0,0,0]])
+R = np.array([[0, 0, 0], [0, 10, 0], [0, 0, 0]])
 
 # define the discount factor
 gamma = 0.9
 
-# define the initial value function
-V = np.zeros((3,3))
-
 # define the probability
-p = 0.8
+prob_moving = 0.8
 
+prob_staying = 1 - prob_moving
 # define the epsilon
 epsilon = 0.0001
 
+
 def value_iteration():
     while True:
-        # store the old value function
-        V_old = np.copy(V)
-        # go through each state
-        for i in range(3):
+        V_old = np.array([[0, 0, 0], [0, 10, 0], [0, 0, 0]], dtype=float)  # store the old value function
+        action_val = []
+        for i in range(3):  # go through each state
             for j in range(3):
-                # calculate the expected value of each action
                 action_val = []
-                if i > 0:
-                    action_val.append(p * (R[i-1, j] + gamma * V_old[i-1, j]))
+                # calculate the expected value of each action
                 if i < 2:
-                    action_val.append(p * (R[i+1, j] + gamma * V_old[i+1, j]))
-                if j > 0:
-                    action_val.append(p * (R[i, j-1] + gamma * V_old[i, j-1]))
+                    action_val.append(prob_moving * (R[i + 1, j] + prob_staying * V_old[i, j]))  # south
+                if i > 0:
+                    action_val.append(prob_moving * (R[i - 1, j] + prob_staying * V_old[i, j]))  # north
                 if j < 2:
-                    action_val.append(p * (R[i, j+1] + gamma * V_old[i, j+1]))
-                action_val.append(p * (R[i, j] + gamma * V_old[i, j]))
-                # find the best action
-                V[i, j] = max(action_val)
-                # find the optimal policy
-                pi[i, j] = np.argmax(action_val)
-        # check if the values are converging
-        if np.sum(np.abs(V - V_old)) <= epsilon:
+                    action_val.append(prob_moving * (R[i, j + 1] + prob_staying * V_old[i, j]))  # east
+                if j > 0:
+                    action_val.append(prob_moving * (R[i, j - 1] + prob_staying * V_old[i, j]))  # west
+                V_old[i][j] = max(action_val)
+        print(V_old)
+        break
+        # take highest from list, put into new matrix
+        # use new matrix as old matrix
+
+def value_iteration2(R, gamma, prob_moving, epsilon):
+    V_old = np.array([[0, 0, 0], [0, 10, 0], [0, 0, 0]], dtype=float)  # store the old value function
+    V_old_prev = np.copy(V_old)
+    while True:
+        action_val = []
+        for i in range(3):  # go through each state
+            for j in range(3):
+                action_val = []
+                # calculate the expected value of each action
+                if i < 2:
+                    action_val.append(prob_moving * (R[i + 1, j] + gamma * V_old[i, j]))  # south
+                if i > 0:
+                    action_val.append(prob_moving * (R[i - 1, j] + gamma * V_old[i, j]))  # north
+                if j < 2:
+                    action_val.append(prob_moving * (R[i, j + 1] + gamma * V_old[i, j]))  # east
+                if j > 0:
+                    action_val.append(prob_moving * (R[i, j - 1] + gamma * V_old[i, j]))  # west
+                V_old[i][j] = max(action_val)
+        if np.all(np.abs(V_old - V_old_prev) < epsilon):  # check convergence
             break
-    # print the optimal value function
-    print("Optimal value function:")
-    print(V)
-    # print the optimal policy
-    print("Optimal policy:")
-    print(pi)
+        else:
+            V_old_prev = np.copy(V_old)
+    return V_old
 
 if __name__ == "__main__":
-    value_iteration()
+    #value_iteration()
+    print(value_iteration2(R, 0.9, 0.8, 0.0001))
