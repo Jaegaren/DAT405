f2ba7c43b3ce0e5af12471605bbaa41b4e51114a
Gustav Gille <gustav.gille@icloud.com>
2023-02-21 15:38:04 +0100

    Första uppgiften löst 2a

--- AIassignment5/main5.1.py
+++ AIassignment5/main5.1.py
@@ -10,56 +10,43 @@ prob_moving = 0.8
 prob_staying = 1 - prob_moving
 # define the epsilon
 
-epsilon = 0.001
+epsilon = 0.01
 
 
 def value_iteration():
-    curMatrix = np.array([[0, 0, 0], [0, 10, 0], [0, 0, 0]], dtype=float)  # store the old value function
     prevMatrix = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]], dtype=float)
-    count = 0
-    diff = float('inf')
-    V_new = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]], dtype=float)
-    # first iteration is special because no backtracking
-    for i in range(3):  # go through each state
-        for j in range(3):
-            action_val = []  # temporary for creating the transition values
-            if i < 2:
-                action_val.append((prob_moving * (curMatrix[i + 1, j]) + prob_staying * curMatrix[i, j]))  # south
-            if i > 0:
-                action_val.append((prob_moving * (curMatrix[i - 1, j]) + prob_staying * curMatrix[i, j]))  # north
-            if j < 2:
-                action_val.append((prob_moving * (curMatrix[i, j + 1]) + prob_staying * curMatrix[i, j]))  # east
-            if j > 0:
-                action_val.append((prob_moving * (curMatrix[i, j - 1]) + prob_staying * curMatrix[i, j]))  # west
-            V_new[i][j] = max(action_val)
-    prevMatrix = curMatrix
-    curMatrix = V_new.copy()
-    print('prevMatrix: ')
-    print(prevMatrix)
-    print('curMatrix: ')
-    print(curMatrix)
-    while diff > epsilon:
+    curMatrix = np.array([[0, 0, 0], [0, 10, 0], [0, 0, 0]], dtype=float)
+    V_new = np.zeros((3, 3))
+    for k in range(10):
         for i in range(3):  # go through each state
             for j in range(3):
                 action_val = []  # temporary for creating the transition values
                 if i < 2:
-                    action_val.append(
-                        (prob_moving * gamma * (curMatrix[i + 1, j]) + prob_staying * gamma * prevMatrix[i, j]))  # south
+                    action_val.append(calculatePositionValue(i + 1, j, i, j, gamma, prevMatrix, curMatrix, prob_moving))
+                    # south
                 if i > 0:
-                    action_val.append(
-                        (prob_moving * gamma * (curMatrix[i - 1, j]) + prob_staying * gamma * prevMatrix[i, j]))  # north
+                    action_val.append(calculatePositionValue(i - 1, j, i, j, gamma, prevMatrix, curMatrix, prob_moving))
+                    # north
                 if j < 2:
-                    action_val.append(
-                        (prob_moving * gamma * (curMatrix[i, j + 1]) + prob_staying * gamma * prevMatrix[i, j]))  # east
+                    action_val.append(calculatePositionValue(i, j + 1, i, j, gamma, prevMatrix, curMatrix, prob_moving))
+                    # east
                 if j > 0:
-                    action_val.append(
-                        (prob_moving * gamma * (curMatrix[i, j - 1]) + prob_staying * gamma * prevMatrix[i, j]))  # west
+                    action_val.append(calculatePositionValue(i, j - 1, i, j, gamma, prevMatrix, curMatrix, prob_moving))
+                    # west
                 V_new[i][j] = max(action_val)
-        diff = np.sum(np.abs(V_new - curMatrix))
-        prevMatrix = curMatrix
-        curMatrix = V_new
-        print(curMatrix)
+        prevMatrix = V_new.copy()
+        print(prevMatrix)
+
+
+
+def calculatePositionValue(directionX, directionY, x, y, gammaG, oldMatrix, currentMatrix, probMove):
+    firstEquationPart = \
+        probMove * ((gammaG * oldMatrix[directionX][directionY]) + currentMatrix[directionX][directionY])
+    secondEquationPart = ((1 - probMove) * ((oldMatrix[x][y] * gammaG) + (currentMatrix[x][y])))
+    return firstEquationPart + secondEquationPart
+
 
 if __name__ == '__main__':
     value_iteration()
+
 
